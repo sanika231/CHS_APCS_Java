996d4921e6e00cfcad1b8ebacc296b2f3b1b97b7
sanika231 <sthatte103@gmail.com>
2026-06-03 18:41:33 -0700

    Add files via upload

---
+++ Game_Projects/Snakes_And_Ladders_Game/src/module-info.java
@@ -0,0 +1,9 @@
+open module P3_T9_Arcade_Engine {
+	requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.desktop;
+    requires javafx.media;
+    
+    //opens engine to javafx.graphics;
+}
